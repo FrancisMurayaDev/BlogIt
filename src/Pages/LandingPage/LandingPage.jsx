a5caaf4fdfe2771342dcfdeab7424285dcd3f739
FrancisMurayaDev <francismurayadev@gmail.com>
2025-04-11 00:53:38 +0300

    set landing page in pages

--- src/Pages/LandingPage/LandingPage.jsx
+++ src/Pages/LandingPage/LandingPage.jsx
@@ -1,9 +1,11 @@
 import NavBar from "../../Components/NavBar/NavBar";
+import { Box, Typography, Button, Container, Stack } from "@mui/material";
 
 function LandingPage() {
   return (
     <>
     <NavBar />
+  
       
     </>
   )

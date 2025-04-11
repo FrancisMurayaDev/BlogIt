92a86b1f919e460e523a338b6cc7e1879e1179de
FrancisMurayaDev <francismurayadev@gmail.com>
2025-04-11 12:33:42 +0300

    format code

--- src/Pages/ArticlePage/ArticlePage.jsx
+++ src/Pages/ArticlePage/ArticlePage.jsx
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from "react";
 
 function ArticlePage() {
-  return (
-    <div>
-      This is the article page
-    </div>
-  )
+  return <div>This is the article page</div>;
 }
 
-export default ArticlePage
+export default ArticlePage;

aadd0fa21858f57d2188b1eb37ad5ecec89ca939
FrancisMurayaDev <francismurayadev@gmail.com>
2025-04-11 11:37:39 +0300

    build the blogs page and blogs UI sample

--- src/Components/BlogCard/BlogCard.jsx
+++ src/Components/BlogCard/BlogCard.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import {
+  Card,
+  CardMedia,
+  CardContent,
+  Typography,
+  Avatar,
+  Box,
+  Button,
+} from "@mui/material";
 
-function BlogCard() {
+function BlogCard({ blog }) {
   return (
-    <div>
-      This is a Blog Card
-    </div>
-  )
+    <Card sx={{ maxWidth: 600, margin: "auto", mb: 4 }}>
+      <CardMedia component="img" height="200" image={blog.image} alt={blog.title} />
+      <CardContent>
+        <Typography variant="h5" gutterBottom>
+          {blog.title}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {blog.excerpt}
+        </Typography>
+        <Box mt={2} display="flex" alignItems="center" justifyContent="space-between">
+          <Box display="flex" alignItems="center">
+            <Avatar src={blog.authorAvatar} sx={{ mr: 1 }} />
+            <Typography variant="body2">{blog.author}</Typography>
+          </Box>
+          <Typography variant="caption">{blog.updatedAt}</Typography>
+        </Box>
+        <Button href={`/blog/${blog.id}`} sx={{ mt: 2 }}>
+          Read More
+        </Button>
+      </CardContent>
+    </Card>
+  );
 }
 
-export default BlogCard
+export default BlogCard;
+
--- src/Pages/BlogListingPage/BlogListingPage.jsx
+++ src/Pages/BlogListingPage/BlogListingPage.jsx
@@ -1,12 +1,42 @@
-import React from 'react'
-import BlogCard from '../../Components/BlogCard/BlogCard'
+import { Container, Typography } from "@mui/material";
+import NavBar from "../../Components/NavBar/NavBar";
+import BlogCard from "../../Components/BlogCard/BlogCard";
+// import img from "../../../public/hero-bg.jpg"
 
-function BlogListingPage() {
+const mockBlogs = [
+  {
+    id: 1,
+    title: "My First Blog Post",
+    excerpt: "This is a quick look into my writing journey...",
+    image: "../../../public/hero-bg.jpg",
+    author: "FrancisDev",
+    authorAvatar: "",
+    updatedAt: "Apr 10, 2025",
+  },
+  {
+    id: 2,
+    title: "Lessons from Full Stack Development",
+    excerpt: "What I’ve learned building modern web apps...",
+    image: "../../../public/hero-bg.jpg",
+    author: "FrancisDev",
+    authorAvatar: "",
+    updatedAt: "Apr 8, 2025",
+  },
+];
+
+export default function BlogListingPage() {
   return (
-    <div>
-      <BlogCard />
-    </div>
-  )
+    <>
+      <NavBar />
+      <Container sx={{ mt: 5 }}>
+        <Typography variant="h4" align="center" gutterBottom>
+          Latest Blogs
+        </Typography>
+        {mockBlogs.map((blog) => (
+          <BlogCard key={blog.id} blog={blog} />
+        ))}
+      </Container>
+    </>
+  );
 }
 
-export default BlogListingPage
